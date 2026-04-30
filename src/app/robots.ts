d5abb5918f89b915c963aa499447463a4cae5fec
kamaeepk <bc240408434mwa@vu.edu.pk>
2026-04-30 17:21:03 +0500

    Update robots.ts file content

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,29 @@ import { SITE_URL } from "@/lib/utils/metadata";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/api/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
